c39dfbc902d67756b9c8fd77cce5ed1aeb5b8447
Ali Jafari <ali1dc@gmail.com>
2011-11-22 22:56:37 -0500

    add scaffold product and styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+Product.delete_all
+
+Product.create(:title => 'Programming Ruby 1.9',
+              :description =>
+          %{<p> Ruby is fastest growing and most exciting dynamic language.</p>},
+          :image_url => '/images/ruby.jpg',
+          :price => 49.50
+)
+
+Product.create(:title => 'Web Design for Developers',
+              :description =>
+          %{<p><em>Web Design for Developers</em> will show you how to make your web-based application look
+            professionally designed. We’ll help you learn how to pick the right colors and fonts, avoid
+            costly interface and accessibility mistakes—your application will really come alive.
+            We’ll also walk you through some common Photoshop and CSS techniques and work through a
+            web site redesign, taking a new design from concept all the way to implementation.</p>},
+          :image_url => '/images/wd4d.jpg',
+          :price => 42.95
+)
